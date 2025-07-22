6fee49081c24eaa2f296d2208a1668b8bb829d2f
void <wordhh1@gmail.com>
2025-07-21 18:13:24 -0600

    Update to Minecraft 1.21.5, Java 21, Gradle 8.14, Loom 1.11, Yarn, Fabric Loader 0.16.14, Fabric API 0.128.1+1.21.5, YACL 3.7.1, Mod Menu 14.0.0-rc.2, Midnight Lib 1.7.3. Update code to reflect API changes.

--- src/main/java/tektonikal/customtotemparticles/mixin/TotemParticleMixin.java
+++ src/main/java/tektonikal/customtotemparticles/mixin/TotemParticleMixin.java
@@ -72,7 +72,7 @@ public abstract class TotemParticleMixin extends AnimatedParticle {
                 }
                 if (YACLConfig.CONFIG.instance().useRotation) {
                     angle = SafeRandom(YACLConfig.CONFIG.instance().minStartRotation, YACLConfig.CONFIG.instance().maxStartRotation);
-                    prevAngle = SafeRandom(YACLConfig.CONFIG.instance().minStartRotation, YACLConfig.CONFIG.instance().maxStartRotation);
+                    lastAngle = SafeRandom(YACLConfig.CONFIG.instance().minStartRotation, YACLConfig.CONFIG.instance().maxStartRotation);
                 }
             }
             if (YACLConfig.CONFIG.instance().useScale) {
@@ -165,14 +165,14 @@ public abstract class TotemParticleMixin extends AnimatedParticle {
         prevRed2 = red2;
         prevGreen2 = green2;
         prevBlue2 = blue2;
-        prevPosX = x;
-        prevPosY = y;
-        prevPosZ = z;
-        prevAngle = angle;
+        lastX = x;
+        lastY = y;
+        lastZ = z;
+        lastAngle = angle;
         prevScale = scale;
         velocityY -= 0.04 * (double) gravityStrength;
         move(velocityX, velocityY, velocityZ);
-        if (ascending && y == prevPosY) {
+        if (ascending && y == lastY) {
             velocityX *= 1.1;
             velocityZ *= 1.1;
         }
@@ -336,12 +336,12 @@ public abstract class TotemParticleMixin extends AnimatedParticle {
     @Override
     public void render(VertexConsumer vertexConsumer, Camera camera, float tickDelta) {
         Vec3d vec3d = camera.getPos();
-        float f = (float) (MathHelper.lerp(tickDelta, this.prevPosX, this.x) - vec3d.getX());
-        float g = (float) (MathHelper.lerp(tickDelta, this.prevPosY, this.y) - vec3d.getY());
-        float h = (float) (MathHelper.lerp(tickDelta, this.prevPosZ, this.z) - vec3d.getZ());
+        float f = (float) (MathHelper.lerp(tickDelta, this.lastX, this.x) - vec3d.getX());
+        float g = (float) (MathHelper.lerp(tickDelta, this.lastY, this.y) - vec3d.getY());
+        float h = (float) (MathHelper.lerp(tickDelta, this.lastZ, this.z) - vec3d.getZ());
         this.getRotator().setRotation(rotation, camera, tickDelta);
         if (this.angle != 0.0F) {
-            this.rotation.rotateZ(MathHelper.lerp(tickDelta, this.prevAngle, this.angle));
+            this.rotation.rotateZ(MathHelper.lerp(tickDelta, this.lastAngle, this.angle));
         }
 
         Vector3f[] vector3fs = new Vector3f[]{new Vector3f(1.0F, -1.0F, 0.0F), new Vector3f(1.0F, 1.0F, 0.0F), new Vector3f(-1.0F, 1.0F, 0.0F), new Vector3f(-1.0F, -1.0F, 0.0F)};
